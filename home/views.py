b776a387cf22b33079dba1a406e7252c9170528d
erol <hurkanrol15@gmail.com>
2021-08-03 00:37:04 +0300

    added model and form

--- home/views.py
+++ home/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 def home(request):
-    return render(request, 'index.html')
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            form = ContactForm()
+    else:
+        form = ContactForm()
+
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'index.html', context)
 
 def about(request):
     return render(request, 'about.html')
