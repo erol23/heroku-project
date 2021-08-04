590450ddf50e48f3ba32bd2054dd9932b46034fa
erol <hurkanrol15@gmail.com>
2021-08-04 21:34:41 +0300

    added media folder

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, Teacher
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ['name', 'phone_number', 'email', 'message']
 
 admin.site.register(Contact, ContactAdmin)
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'email', 'speciality']
+
+admin.site.register(Teacher, TeacherAdmin)
--- home/models.py
+++ home/models.py
@@ -10,3 +10,21 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to = 'teacher', default = "default.png")
+    email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=50)
+
+    TITLE = (
+        ('english', 'English Teacher'),
+        ('Scratch', 'Scratch Teacher'),
+        ('Science', 'Science Teacher'),
+        ('Sport', 'Sport Teacher'),
+    )
+
+    speciality = models.CharField(max_length=50, choices=TITLE)
+
+    def __str__(self):
+        return self.name
